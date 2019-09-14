6dc58ca789c8a55ddfc7a54fb0ba468d376014a2
junyuan.zeng <junyuan.zeng@jd.com>
2019-09-13 18:17:46 -0700

    done the webserver flow

--- authnode/api_service.go
+++ authnode/api_service.go
@@ -1,1 +1,68 @@
 package authnode
+
+import (
+  "net/http"
+  "encoding/json"
+  "strconv"
+
+  //"github.com/chubaofs/chubaofs/util/errors"
+	"github.com/chubaofs/chubaofs/util/log"
+  "github.com/chubaofs/chubaofs/proto"
+)
+
+func (m *Server) extractClientInfo(r *http.Request) (name string, target string, ip string, ts string, err error) {
+  return
+}
+
+func (m *Server) getTicket(w http.ResponseWriter, r *http.Request) {
+  /*var (
+		name    string
+		target    string
+	)*/
+
+	sendOkReply(w, r, newSuccessHTTPReply("Hello World!"))
+}
+
+func newSuccessHTTPReply(data interface{}) *proto.HTTPReply {
+	return &proto.HTTPReply{Code: proto.ErrCodeSuccess, Msg: proto.ErrSuc.Error(), Data: data}
+}
+
+func sendOkReply(w http.ResponseWriter, r *http.Request, httpReply *proto.HTTPReply) (err error) {
+	/*switch httpReply.Data.(type) {
+	case *DataPartition:
+		dp := httpReply.Data.(*DataPartition)
+		dp.RLock()
+		defer dp.RUnlock()
+	case *MetaPartition:
+		mp := httpReply.Data.(*MetaPartition)
+		mp.RLock()
+		defer mp.RUnlock()
+	case *MetaNode:
+		mn := httpReply.Data.(*MetaNode)
+		mn.RLock()
+		defer mn.RUnlock()
+	case *DataNode:
+		dn := httpReply.Data.(*DataNode)
+		dn.RLock()
+		defer dn.RUnlock()
+	}*/
+	reply, err := json.Marshal(httpReply)
+	if err != nil {
+		log.LogErrorf("fail to marshal http reply[%v]. URL[%v],remoteAddr[%v] err:[%v]", httpReply, r.URL, r.RemoteAddr, err)
+		http.Error(w, "fail to marshal http reply", http.StatusBadRequest)
+		return
+	}
+	send(w, r, reply)
+	return
+}
+
+func send(w http.ResponseWriter, r *http.Request, reply []byte) {
+	w.Header().Set("content-type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(reply)))
+	if _, err := w.Write(reply); err != nil {
+		log.LogErrorf("fail to write http reply[%s] len[%d].URL[%v],remoteAddr[%v] err:[%v]", string(reply), len(reply), r.URL, r.RemoteAddr, err)
+		return
+	}
+	log.LogInfof("URL[%v],remoteAddr[%v],response ok", r.URL, r.RemoteAddr)
+	return
+}
--- authnode/http_server.go
+++ authnode/http_server.go
@@ -3,6 +3,7 @@ package authnode
 import (
 	"net/http"
 	"github.com/chubaofs/chubaofs/util/log"
+	"github.com/chubaofs/chubaofs/proto"
 )
 
 func (m *Server) startHTTPService() {
@@ -13,5 +14,54 @@ func (m *Server) startHTTPService() {
 			panic(err)
 		}
 	}()
+	return
+}
+
+/*func (m *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.LogInfof("URL[%v],remoteAddr[%v]", r.URL, r.RemoteAddr)
+	switch r.URL.Path {
+	case proto.ClientGetTicket:
+		m.get(w, r)
+	default:
+
+	}
+}*/
+
+func (m *Server) handleFunctions() {
+	/*
+	http.HandleFunc(proto.AdminGetIP, m.getIPAddr)
+	http.Handle(proto.AdminGetCluster, m.handlerWithInterceptor())
+	http.Handle(proto.AdminGetDataPartition, m.handlerWithInterceptor())
+	http.Handle(proto.AdminCreateDataPartition, m.handlerWithInterceptor())
+	http.Handle(proto.AdminLoadDataPartition, m.handlerWithInterceptor())
+	http.Handle(proto.AdminDecommissionDataPartition, m.handlerWithInterceptor())
+	http.Handle(proto.AdminCreateVol, m.handlerWithInterceptor())
+	http.Handle(proto.AdminGetVol, m.handlerWithInterceptor())
+	http.Handle(proto.AdminDeleteVol, m.handlerWithInterceptor())
+	http.Handle(proto.AdminUpdateVol, m.handlerWithInterceptor())
+	http.Handle(proto.AdminClusterFreeze, m.handlerWithInterceptor())
+	http.Handle(proto.AddDataNode, m.handlerWithInterceptor())
+	http.Handle(proto.AddMetaNode, m.handlerWithInterceptor())
+	http.Handle(proto.DecommissionDataNode, m.handlerWithInterceptor())
+	http.Handle(proto.DecommissionDisk, m.handlerWithInterceptor())
+	http.Handle(proto.DecommissionMetaNode, m.handlerWithInterceptor())
+	http.Handle(proto.GetDataNode, m.handlerWithInterceptor())
+	http.Handle(proto.GetMetaNode, m.handlerWithInterceptor())
+	http.Handle(proto.AdminLoadMetaPartition, m.handlerWithInterceptor())
+	http.Handle(proto.AdminDecommissionMetaPartition, m.handlerWithInterceptor())
+	http.Handle(proto.ClientDataPartitions, m.handlerWithInterceptor())
+	http.Handle(proto.ClientVol, m.handlerWithInterceptor())
+	http.Handle(proto.ClientMetaPartitions, m.handlerWithInterceptor())
+	http.Handle(proto.ClientMetaPartition, m.handlerWithInterceptor())
+	http.Handle(proto.GetDataNodeTaskResponse, m.handlerWithInterceptor())
+	http.Handle(proto.GetMetaNodeTaskResponse, m.handlerWithInterceptor())
+	http.Handle(proto.AdminCreateMP, m.handlerWithInterceptor())
+	http.Handle(proto.ClientVolStat, m.handlerWithInterceptor())
+	http.Handle(proto.AddRaftNode, m.handlerWithInterceptor())
+	http.Handle(proto.RemoveRaftNode, m.handlerWithInterceptor())
+	http.Handle(proto.AdminSetMetaNodeThreshold, m.handlerWithInterceptor())
+	http.Handle(proto.GetTopologyView, m.handlerWithInterceptor())
+*/
+	http.HandleFunc(proto.ClientGetTicket, m.getTicket)
 	return
 }
