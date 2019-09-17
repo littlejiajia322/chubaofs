95dc444e83de39474e8c6b2df20ebc1650e5dd6d
junyuan.zeng <junyuan.zeng@jd.com>
2019-09-16 18:24:08 -0700

    before testing crypto functions

--- authnode/api_service.go
+++ authnode/api_service.go
@@ -1,6 +1,9 @@
 package authnode
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"strconv"
@@ -8,21 +11,11 @@ import (
 
 	//"github.com/chubaofs/chubaofs/util/errors"
 	"github.com/chubaofs/chubaofs/proto"
+	"github.com/chubaofs/chubaofs/util/cryptoutil"
 	"github.com/chubaofs/chubaofs/util/errors"
 	"github.com/chubaofs/chubaofs/util/log"
 
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/md5"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/binary"
-	"encoding/hex"
 	"fmt"
-	"io"
-	rand2 "math/rand"
-	"time"
 )
 
 func keyNotFound(name string) (err error) {
@@ -58,6 +51,39 @@ func (m *Server) extractClientInfo(r *http.Request) (client string, target strin
 	if target = r.FormValue(targetService); target == "" {
 		err = keyNotFound(targetService)
 		return
+	}
+	return
+}
+
+func verifyMessage(message string, key []byte) (err error) {
+	var (
+		cipher    []byte
+		plaintext []byte
+	)
+
+	if cipher, err = base64.StdEncoding.DecodeString(message); err != nil {
+		return
+	}
+
+	if plaintext, err = cryptoutil.AesDecryptCBC(key, cipher); err != nil {
+		return
+	}
+
+	//plaintext := Unpad(paddingtext)
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+
+	checksum2 := make([]byte, 16)
+	copy(checksum2, plaintext[8:24])
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
+	filltext := bytes.Repeat([]byte{byte(0)}, 16)
+	copy(plaintext[8:], filltext[:])
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+	checksum3 := md5.Sum(plaintext)
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum3[:]))
+
+	if bytes.Compare(checksum2, checksum3[:]) != 0 {
+		panic("not equal")
 	}
 	return
 }
@@ -101,7 +127,7 @@ func newSuccessHTTPReply(data interface{}) *proto.HTTPReply {
 func sendOkReply(w http.ResponseWriter, r *http.Request, httpReply *proto.HTTPReply) (err error) {
 	/*switch httpReply.Data.(type) {
 	case *DataPartition:
-		dp := httpReply.Data.(*DataPartition)
+		dp := httpReply.Data.(*DataPartition)Block
 		dp.RLock()
 		defer dp.RUnlock()
 	case *MetaPartition:
@@ -136,130 +162,4 @@ func send(w http.ResponseWriter, r *http.Request, reply []byte) {
 	}
 	log.LogInfof("URL[%v],remoteAddr[%v],response ok", r.URL, r.RemoteAddr)
 	return
-}
-
-func Pad(src []byte) []byte {
-	padding := aes.BlockSize - len(src)%aes.BlockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(src, padtext...)
-}
-
-func Unpad(src []byte) []byte {
-	length := len(src)
-	unpadding := int(src[length-1])
-	return src[:(length - unpadding)]
-}
-
-// CBC
-func encryptCBC(key, plaintext []byte) (ciphertext []byte, err error) {
-	if len(plaintext)%aes.BlockSize != 0 {
-		panic("plaintext is not a multiple of the block size")
-	}
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	ciphertext = make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-	//iv, _ := hex.DecodeString("acfa7a047800b2f221f2c4f7d626eafb")
-	//copy(ciphertext[:aes.BlockSize], iv)
-
-	fmt.Printf("CBC Key: %s\n", hex.EncodeToString(key))
-	fmt.Printf("CBC IV: %s\n", hex.EncodeToString(iv))
-
-	cbc := cipher.NewCBCEncrypter(block, iv)
-	cbc.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
-
-	return
-}
-
-func decryptCBC(key, ciphertext []byte) (plaintext []byte, err error) {
-	var block cipher.Block
-
-	if block, err = aes.NewCipher(key); err != nil {
-		return
-	}
-
-	if len(ciphertext) < aes.BlockSize {
-		fmt.Printf("ciphertext too short")
-		return
-	}
-
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	cbc := cipher.NewCBCDecrypter(block, iv)
-	cbc.CryptBlocks(ciphertext, ciphertext)
-
-	plaintext = ciphertext
-
-	return
-}
-
-func createReq2(req []byte) (message string) {
-	var cipher []byte
-	var err error
-	key := []byte("11111111111111111111111111111111")
-
-	plaintext := make([]byte, 8+16+8+len(req))
-	random := rand2.Uint64()
-
-	// add random
-	binary.LittleEndian.PutUint64(plaintext, random)
-	// add timestamp
-	binary.LittleEndian.PutUint64(plaintext[8+16:], uint64(time.Now().Unix()))
-	// add request body
-	copy(plaintext[8+16+8:], req)
-	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
-	checksum := md5.Sum(plaintext)
-	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum[:]))
-	copy(plaintext[8:], checksum[:])
-	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
-
-	if cipher, err = encryptCBC(key, Pad(plaintext)); err != nil {
-		panic(err)
-	}
-
-	message = base64.StdEncoding.EncodeToString(cipher)
-	fmt.Printf("CBC: %s\n", message)
-
-	return
-}
-
-func checkMessage2(message string) {
-	var cipher []byte
-	var err error
-	key := []byte("11111111111111111111111111111111")
-	var paddingtext []byte
-
-	if cipher, err = base64.StdEncoding.DecodeString(message); err != nil {
-		panic(err)
-	}
-
-	if paddingtext, err = decryptCBC(key, cipher); err != nil {
-		panic(err)
-	}
-
-	plaintext := Unpad(paddingtext)
-	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
-
-	checksum2 := make([]byte, 16)
-	copy(checksum2, plaintext[8:24])
-	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
-	filltext := bytes.Repeat([]byte{byte(0)}, 16)
-	copy(plaintext[8:], filltext[:])
-	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
-	checksum3 := md5.Sum(plaintext)
-	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
-	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum3[:]))
-
-	if bytes.Compare(checksum2, checksum3[:]) != 0 {
-		panic("not equal")
-	}
-
 }
--- client.go
+++ client.go
@@ -1,31 +1,194 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strings"
+	"io"
+	rand2 "math/rand"
+	"time"
 )
 
-// keep first n lines
-func keepLines(s string, n int) string {
-	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
-	return strings.Replace(result, "\r", "", -1)
+func Pad(src []byte) []byte {
+	padding := aes.BlockSize - len(src)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+func Unpad(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
+}
+
+// CBC
+func encryptCBC(key, plaintext []byte) (ciphertext []byte, err error) {
+	var (
+		block cipher.Block
+	)
+
+	if plaintext == nil || len(plaintext) == 0 {
+		err = fmt.Errorf("input for encryption is invalid")
+		return
+	}
+
+	paddedText := Pad(plaintext)
+
+	if len(paddedText)%aes.BlockSize != 0 {
+		err = fmt.Errorf("paddedText [len=%d] is not a multiple of the block size", len(paddedText))
+		return
+	}
+
+	block, err = aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+
+	ciphertext = make([]byte, aes.BlockSize+len(paddedText))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return
+	}
+
+	//fmt.Printf("CBC Key: %s\n", hex.EncodeToString(key))
+	//fmt.Printf("CBC IV: %s\n", hex.EncodeToString(iv))
+
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	cbc.CryptBlocks(ciphertext[aes.BlockSize:], paddedText)
+
+	return
+}
+
+func decryptCBC(key, ciphertext []byte) (plaintext []byte, err error) {
+	var block cipher.Block
+
+	if block, err = aes.NewCipher(key); err != nil {
+		return
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		err = fmt.Errorf("ciphertext [len=%d] too short", len(ciphertext))
+		return
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	cbc := cipher.NewCBCDecrypter(block, iv)
+	cbc.CryptBlocks(ciphertext, ciphertext)
+
+	plaintext = Unpad(ciphertext)
+
+	return
+}
+
+func createEncReq(req []byte, key []byte) (message string, err error) {
+	var cipher []byte
+
+	// 8 for random number; 16 for md5 hash
+	plaintext := make([]byte, 8+16+len(req))
+	// add random
+	random := rand2.Uint64()
+	binary.LittleEndian.PutUint64(plaintext, random)
+	// add request body
+	copy(plaintext[8+16:], req)
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+	// calculate and add md5
+	checksum := md5.Sum(plaintext)
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum[:]))
+	copy(plaintext[8:], checksum[:])
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+	// encryption with aes CBC with keysize of 256-bit
+	if cipher, err = encryptCBC(key, plaintext); err != nil {
+		return
+	}
+	// base64 encoding
+	message = base64.StdEncoding.EncodeToString(cipher)
+	fmt.Printf("CBC: %s\n", message)
+	return
+}
+
+func verifyMessage(message string, key []byte) (err error) {
+	var (
+		cipher    []byte
+		plaintext []byte
+	)
+
+	if cipher, err = base64.StdEncoding.DecodeString(message); err != nil {
+		return
+	}
+
+	if plaintext, err = decryptCBC(key, cipher); err != nil {
+		return
+	}
+
+	//plaintext := Unpad(paddingtext)
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+
+	checksum2 := make([]byte, 16)
+	copy(checksum2, plaintext[8:24])
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
+	filltext := bytes.Repeat([]byte{byte(0)}, 16)
+	copy(plaintext[8:], filltext[:])
+	fmt.Printf("plaintext=%s %d\n", base64.StdEncoding.EncodeToString(plaintext), len(plaintext))
+	checksum3 := md5.Sum(plaintext)
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum2))
+	fmt.Printf("checksum=%s\n", base64.StdEncoding.EncodeToString(checksum3[:]))
+
+	if bytes.Compare(checksum2, checksum3[:]) != 0 {
+		panic("not equal")
+	}
+	return
+}
+
+type authEncReq struct {
+	clientID string
+	service  string
+	ip       string
+	ts       int64
 }
 
 func main() {
-	// We can use POST form to get result, too.
-	resp, err := http.PostForm("http://localhost:8080/client/getticket",
-		url.Values{"clientID": {"client1"}, "service": {"master"}})
+	var (
+		err     error
+		message string
+	)
+	clientID := "client1"
+	service := "master"
+	ip := "123456789"
+	ts := time.Now().Unix()
+	// construct request body
+	message_struct := authEncReq{clientID: clientID, service: service, ip: ip, ts: ts}
+
+	// encrption
+	message, err = createEncReq([]byte(fmt.Sprintf("%v", message_struct)), []byte("11111111111111111111111111111111"))
+
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
-	body, err2 := ioutil.ReadAll(resp.Body)
-	if err2 != nil {
-		panic(err2)
+
+	if err = verifyMessage(message, []byte("11111111111111111111111111111111")); err != nil {
+		panic(err)
 	}
-	//fmt.Println("post:\n", keepLines(string(body), 3))
-	fmt.Println("respose: %s", string(body))
+	/*
+		// We can use POST form to get result, too.
+		resp, err := http.PostForm("http://localhost:8081/client/getticket",
+			url.Values{"clientID": {"client1"}, "message": {message}})
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err2 := ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			panic(err2)
+		}
+		//fmt.Println("post:\n", keepLines(string(body), 3))
+		fmt.Printf("respose: %s", string(body))*/
+
+	return
 }
